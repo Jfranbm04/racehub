88bf6cf0c9fec960f3943356367fc05164b3a9a1
Jfranbm04 <juanfranburgos2004@gmail.com>
2025-03-05 16:45:36 +0100

    EDIT:RunningJSON

--- backend/src/Entity/Running.php
+++ backend/src/Entity/Running.php
@@ -7,8 +7,8 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Serializer\Annotation\MaxDepth;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\MaxDepth;
 
 #[ORM\Entity(repositoryClass: RunningRepository::class)]
 class Running
@@ -20,51 +20,22 @@ class Running
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(["running:read", "running_participant:read"])]
+    #[Groups(["running:read"])]
     private ?string $name = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    #[Groups(["running:read", "running_participant:read"])]
-    private ?string $description = null;
-
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
-    #[Groups(["running:read", "running_participant:read"])]
+    #[Groups(["running:read"])]
     private ?\DateTimeInterface $date = null;
 
-    #[ORM\Column]
-    #[Groups(["running:read", "running_participant:read"])]
-    private ?int $distance_km = null;
-
     #[ORM\Column(length: 255)]
-    #[Groups(["running:read", "running_participant:read"])]
+    #[Groups(["running:read"])]
     private ?string $location = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    #[Groups(["running:read", "running_participant:read"])]
-    private ?string $coordinates = null;
-
-    #[ORM\Column]
-    #[Groups(["running:read", "running_participant:read"])]
-    private ?int $entry_fee = null;
-
-    #[ORM\Column]
-    #[Groups(["running:read", "running_participant:read"])]
-    private ?int $available_slots = null;
-
-    #[ORM\Column(length: 255)]
-    #[Groups(["running:read", "running_participant:read"])]
-    private ?string $status = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    #[Groups(["running:read", "running_participant:read"])]
-    private ?string $category = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    #[Groups(["running:read", "running_participant:read"])]
-    private ?string $image = null;
-
-    #[ORM\OneToMany(targetEntity: RunningParticipant::class, mappedBy: 'running')]
-    #[MaxDepth(1)] // Limita la profundidad de la serialización
+    /**
+     * Relación OneToMany con RunningParticipant
+     */
+    #[ORM\OneToMany(mappedBy: 'running', targetEntity: RunningParticipant::class)]
+    #[MaxDepth(1)]
     #[Groups(["running:read"])]
     private Collection $runningParticipants;
 
@@ -86,17 +57,7 @@ class Running
     public function setName(string $name): static
     {
         $this->name = $name;
-        return $this;
-    }
 
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(?string $description): static
-    {
-        $this->description = $description;
         return $this;
     }
 
@@ -108,17 +69,7 @@ class Running
     public function setDate(\DateTimeInterface $date): static
     {
         $this->date = $date;
-        return $this;
-    }
 
-    public function getDistanceKm(): ?int
-    {
-        return $this->distance_km;
-    }
-
-    public function setDistanceKm(int $distance_km): static
-    {
-        $this->distance_km = $distance_km;
         return $this;
     }
 
@@ -130,72 +81,7 @@ class Running
     public function setLocation(string $location): static
     {
         $this->location = $location;
-        return $this;
-    }
 
-    public function getCoordinates(): ?string
-    {
-        return $this->coordinates;
-    }
-
-    public function setCoordinates(?string $coordinates): static
-    {
-        $this->coordinates = $coordinates;
-        return $this;
-    }
-
-    public function getEntryFee(): ?int
-    {
-        return $this->entry_fee;
-    }
-
-    public function setEntryFee(int $entry_fee): static
-    {
-        $this->entry_fee = $entry_fee;
-        return $this;
-    }
-
-    public function getAvailableSlots(): ?int
-    {
-        return $this->available_slots;
-    }
-
-    public function setAvailableSlots(int $available_slots): static
-    {
-        $this->available_slots = $available_slots;
-        return $this;
-    }
-
-    public function getStatus(): ?string
-    {
-        return $this->status;
-    }
-
-    public function setStatus(string $status): static
-    {
-        $this->status = $status;
-        return $this;
-    }
-
-    public function getCategory(): ?string
-    {
-        return $this->category;
-    }
-
-    public function setCategory(?string $category): static
-    {
-        $this->category = $category;
-        return $this;
-    }
-
-    public function getImage(): ?string
-    {
-        return $this->image;
-    }
-
-    public function setImage(?string $image): static
-    {
-        $this->image = $image;
         return $this;
     }
 
@@ -213,16 +99,19 @@ class Running
             $this->runningParticipants->add($runningParticipant);
             $runningParticipant->setRunning($this);
         }
+
         return $this;
     }
 
     public function removeRunningParticipant(RunningParticipant $runningParticipant): static
     {
         if ($this->runningParticipants->removeElement($runningParticipant)) {
+            // set the owning side to null (unless already changed)
             if ($runningParticipant->getRunning() === $this) {
                 $runningParticipant->setRunning(null);
             }
         }
+
         return $this;
     }
 }
