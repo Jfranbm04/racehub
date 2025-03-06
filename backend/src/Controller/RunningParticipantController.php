0a774c058bb777be1c0ba520c1cd8610e62dd880
Jfranbm04 <juanfranburgos2004@gmail.com>
2025-03-06 18:55:40 +0100

    FIX: metodos de runningParticipantController (de serializer a decode)

--- backend/src/Controller/RunningParticipantController.php
+++ backend/src/Controller/RunningParticipantController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\RunningParticipant;
-use App\Form\RunningParticipantType;
 use App\Repository\RunningParticipantRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,46 +10,124 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api/running_participant')]
 final class RunningParticipantController extends AbstractController
 {
-    #[Route(name: 'app_running_participant_index', methods: ['GET'])]
+    #[Route('', name: 'app_running_participant_index', methods: ['GET'])]
     public function index(RunningParticipantRepository $runningParticipantRepository): JsonResponse
     {
         $participants = $runningParticipantRepository->findAll();
-        return $this->json($participants, Response::HTTP_OK, [], ['groups' => 'running_participant:read']);
+        return $this->json($participants, Response::HTTP_OK, [], [
+            'groups' => [
+                'running_participant:read',
+                'user:read',
+                'running:read'
+            ],
+            'circular_reference_handler' => function ($object) {
+                return $object->getId();
+            }
+        ]);
     }
 
     #[Route('/new', name: 'app_running_participant_new', methods: ['POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    public function new(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
         try {
-            $participant = $serializer->deserialize($request->getContent(), RunningParticipant::class, 'json');
+            $data = json_decode($request->getContent(), true);
+
+            // Create new participant
+            $participant = new RunningParticipant();
+
+            // Get existing entities
+            $user = $entityManager->getReference('App\Entity\User', $data['user']);
+            $running = $entityManager->getReference('App\Entity\Running', $data['running']);
+
+            // Set the relationships
+            $participant->setUser($user);
+            $participant->setRunning($running);
+            $participant->setDorsal($data['dorsal']);
+            $participant->setBanned($data['banned']);
+
+            if (isset($data['time'])) {
+                $participant->setTime(new \DateTime($data['time']));
+            }
+
             $entityManager->persist($participant);
             $entityManager->flush();
 
-            return $this->json($participant, Response::HTTP_CREATED, [], ['groups' => 'running_participant:read']);
+            return $this->json($participant, Response::HTTP_CREATED, [], [
+                'groups' => [
+                    'running_participant:read',
+                    'user:read',
+                    'running:read'
+                ],
+                'circular_reference_handler' => function ($object) {
+                    return $object->getId();
+                }
+            ]);
         } catch (\Exception $e) {
             return $this->json(['error' => $e->getMessage()], Response::HTTP_BAD_REQUEST);
         }
     }
 
     #[Route('/{id}', name: 'app_running_participant_show', methods: ['GET'])]
-    public function show(RunningParticipant $participant): JsonResponse
+    public function show(int $id, RunningParticipantRepository $repository): JsonResponse
     {
-        return $this->json($participant, Response::HTTP_OK, [], ['groups' => 'running_participant:read']);
+        $participant = $repository->find($id);
+
+        if (!$participant) {
+            return $this->json(['error' => 'Participant not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        return $this->json($participant, Response::HTTP_OK, [], [
+            'groups' => [
+                'running_participant:read',
+                'user:read',
+                'running:read'
+            ],
+            'circular_reference_handler' => function ($object) {
+                return $object->getId();
+            }
+        ]);
     }
 
     #[Route('/{id}/edit', name: 'app_running_participant_edit', methods: ['PUT'])]
-    public function edit(Request $request, RunningParticipant $participant, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    public function edit(Request $request, RunningParticipant $participant, EntityManagerInterface $entityManager): JsonResponse
     {
         try {
-            $updatedParticipant = $serializer->deserialize($request->getContent(), RunningParticipant::class, 'json', ['object_to_populate' => $participant]);
+            $data = json_decode($request->getContent(), true);
+
+            if (isset($data['user'])) {
+                $user = $entityManager->getReference('App\Entity\User', $data['user']);
+                $participant->setUser($user);
+            }
+            if (isset($data['running'])) {
+                $running = $entityManager->getReference('App\Entity\Running', $data['running']);
+                $participant->setRunning($running);
+            }
+            if (isset($data['dorsal'])) {
+                $participant->setDorsal($data['dorsal']);
+            }
+            if (isset($data['banned'])) {
+                $participant->setBanned($data['banned']);
+            }
+            if (isset($data['time'])) {
+                $participant->setTime(new \DateTime($data['time']));
+            }
+
             $entityManager->flush();
 
-            return $this->json($updatedParticipant, Response::HTTP_OK, [], ['groups' => 'running_participant:read']);
+            return $this->json($participant, Response::HTTP_OK, [], [
+                'groups' => [
+                    'running_participant:read',
+                    'user:read',
+                    'running:read'
+                ],
+                'circular_reference_handler' => function ($object) {
+                    return $object->getId();
+                }
+            ]);
         } catch (\Exception $e) {
             return $this->json(['error' => $e->getMessage()], Response::HTTP_BAD_REQUEST);
         }
