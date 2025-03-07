824bbc539eb89b78376638b5eafe1d0439b90ba9
IsmaCar <160272744+IsmaCar@users.noreply.github.com>
2025-03-07 13:13:25 +0100

    Modify functions CyclingController

--- backend/src/Controller/CyclingController.php
+++ backend/src/Controller/CyclingController.php
@@ -5,8 +5,13 @@ namespace App\Controller;
 use App\Entity\Cycling;
 use App\Form\CyclingType;
 use App\Repository\CyclingRepository;
+use Doctrine\DBAL\Types\DateType;
+use Doctrine\DBAL\Types\TextType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -88,24 +93,25 @@ final class CyclingController extends AbstractController
     }
 
     #[Route('/new_s', name: 'app_cycling_start', methods: ['POST'])]
-    public function new_s(Cycling $cycling, EntityManagerInterface $entityManager): Response
+    public function new_s(Request $request, EntityManagerInterface $entityManager): Response
     {
         $cycling = new Cycling();
 
-        $cycling->setName('Cycling 1');
-        $cycling->setDescription('Cycling 1 description');
-        $cycling->setDate(new \DateTime());
-        $cycling->setDistanceKm(0);
-        $cycling->setLocation('Cycling 1 location');
-        $cycling->setCoordinates('0,0');
-        $cycling->setUnevenness(0);
-        $cycling->setEntryFee(0);
-        $cycling->setAvailableSlots(0);  
-        $cycling->setStatus(0);
-        $cycling->setCategory('Cycling 1 category');      
-        $cycling->setStatus('In progress');
-        $cycling->setImage('Cycling 1 image url');
+        // Capturando los datos enviados por el formulario
+        $cycling->setName($request->request->get('name'));
+        $cycling->setDescription($request->request->get('description'));
+        $cycling->setDate(new \DateTime($request->request->get('date')));
+        $cycling->setDistanceKm((float)$request->request->get('distance_km'));
+        $cycling->setLocation($request->request->get('location'));
+        $cycling->setCoordinates($request->request->get('coordinates'));
+        $cycling->setUnevenness((int)$request->request->get('unevenness'));
+        $cycling->setEntryFee((float)$request->request->get('entry_fee'));
+        $cycling->setAvailableSlots((int)$request->request->get('available_slots'));
+        $cycling->setStatus($request->request->get('status'));
+        $cycling->setCategory($request->request->get('category'));
+        $cycling->setImage($request->request->get('image'));
 
+        // Guardando en la base de datos
         $entityManager->persist($cycling);
         $entityManager->flush();
 
@@ -114,27 +120,40 @@ final class CyclingController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit_s', name: 'app_cycling_edit', methods: ['PUT'])]
-    public function edit_s(Request $request, Cycling $cycling, EntityManagerInterface $entityManager): Response
-    {
-        $cycling->setName($request->request->get('name'));
-        $cycling->setDescription($request->request->get('description'));
-        $cycling->setDate(new \DateTime($request->request->get('date')));
-        $cycling->setDistanceKm($request->request->get('distance_km'));
-        $cycling->setLocation($request->request->get('location'));
-        $cycling->setCoordinates($request->request->get('coordinates'));
-        $cycling->setUnevenness($request->request->get('unevenness'));
-        $cycling->setEntryFee($request->request->get('entry_fee'));
-        $cycling->setAvailableSlots($request->request->get('available_slots'));
-        $cycling->setStatus($request->request->get('status'));
-        $cycling->setCategory($request->request->get('category'));
-        $cycling->setImage($request->request->get('image'));
 
-        $entityManager->persist($cycling);
-        $entityManager->flush();
+    
+    // #[Route('/{id}/edit_s', name: 'app_cycling_edit', methods: ['GET', 'PUT'])]
+    // public function edit_s(Request $request, Cycling $cycling, EntityManagerInterface $entityManager): Response
+    // {
+    //     // Crear el formulario de edición
+    //     $form = $this->createForm(CyclingType::class, $cycling);
 
-        return $this->redirectToRoute('app_cycling_show', ['id' => $cycling->getId()]);
-    }
+    //     // Si es una solicitud GET, mostrar el formulario con los datos actuales
+    //     if ($request->isMethod('GET')) {
+    //         return $this->render('cycling/edit.html.twig', [
+    //             'form' => $form->createView(),
+    //             'cycling' => $cycling,  // Los datos actuales del ciclo
+    //         ]);
+    //     }
+
+    //     // Si es una solicitud PUT, manejar la actualización
+    //     $form->handleRequest($request);
+
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         // Guardar los cambios en la base de datos
+    //         $entityManager->persist($cycling);
+    //         $entityManager->flush();
+
+    //         // Redirigir al usuario a otra página después de la actualización
+    //         return $this->redirectToRoute('app_admin'); // O la ruta que prefieras
+    //     }
+
+    //     // Si el formulario no es válido, vuelve a mostrarlo con los errores
+    //     return $this->render('cycling/edit.html.twig', [
+    //         'form' => $form->createView(),
+    //         'cycling' => $cycling,
+    //     ]);
+    // }
 
     #[Route('/{id}', name: 'app_cycling_delete', methods: ['DELETE'])]
     public function delete_s(Cycling $cycling, EntityManagerInterface $entityManager): Response
@@ -144,5 +163,4 @@ final class CyclingController extends AbstractController
 
         return $this->redirectToRoute('app_cycling_index');
     }
-
 }
